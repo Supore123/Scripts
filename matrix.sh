03d47c9b4d3e274240267f1194e9f6e9e08102e2
Jonathan Y <jonathanyohannes421@gmail.com>
2026-03-03 14:04:38 +0000

    Define color arrays for dynamic Matrix styling

--- matrix.sh
+++ matrix.sh
@@ -15,3 +15,20 @@ sleep 0.1
 
 USERNAME="Supore123"
 
+# Define lists of cmatrix colors
+CMATRIX_COLORS=("green" "red" "blue" "white" "yellow" "cyan" "magenta")
+ANSI_COLORS=(
+    "\033[1;32m" # green
+    "\033[1;31m" # red
+    "\033[1;34m" # blue
+    "\033[1;37m" # white
+    "\033[1;33m" # yellow
+    "\033[1;36m" # cyan
+    "\033[1;35m" # magenta
+)
+RESET="\033[0m"
+
+RAND_INDEX=$((RANDOM % 7))
+SELECTED_COLOR="${CMATRIX_COLORS[$RAND_INDEX]}"
+SELECTED_ANSI="${ANSI_COLORS[$RAND_INDEX]}"
+
